cb4de9c6e87f4c8a6caf7745fc426e697fc95769
FriedAxons <remo453@gmail.com>
2022-12-30 16:08:44 -0600

    More styles and starting JS file
    On the JS file the containers and buttons potentially needed for the
    active states and work that is going to be done, were grabbed
    with query selectors. Ran into a issue where one can add the
    "active" class to the button, however there is uncertainty on how to
    remove it when a different button is clicked.

---
+++ index.js
@@ -0,0 +1,6 @@
+const containerEl = document.querySelector('.container');
+const buttonRatingContainerEl = document.querySelector('.btn-rating-container');
+const ratingBtnsEl = document.querySelectorAll('.rating-btn');
+const thankYouContainerEl = document.querySelector('.thank-you-container');
+const submitBtnEl = document.querySelector('.submit-btn');
+const selectedRatingEl = document.querySelector('.selected-rating');
