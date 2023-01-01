9ca1da00e0474c7e6354307ba5aeaaac8e38f204
FriedAxons <remo453@gmail.com>
2023-01-01 11:43:56 -0600

    Project completed"

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
-const containerEl = document.querySelector('.container');
+const selectedRating = document.querySelector('.selected-rating');
+
+let button = null;
+
 const btnRatingContainer = document.querySelector('.btn-rating-container');
-const ratingBtns = document.querySelectorAll('.rating-btn');
-const thankYouContainerEl = document.querySelector('.thank-you-container');
-const submitBtn = document.querySelector('.submit-btn');
-const selectedRatingEl = document.querySelector('.selected-rating');
-
 btnRatingContainer.addEventListener('click', (event) => {
-  // Get the clicked button
-  const button = event.target;
+  //   Get the clicked button
+  button = event.target;
 
-  // Remove the active class from all buttons
+  //   Remove the active class from all buttons
+  const ratingBtns = document.querySelectorAll('.rating-btn');
   ratingBtns.forEach((btn) => {
     btn.classList.remove('active');
   });
 
   button.classList.add('active');
+});
+
+const submitBtn = document.querySelector('.submit-btn');
+
+submitBtn.addEventListener('click', (event) => {
+  // Prevent the form from being submitted
+  event.preventDefault();
+
+  const container = document.querySelector('.container');
+  container.classList.add('active');
+
+  const thankYouContainer = document.querySelector('.thank-you-container');
+  thankYouContainer.classList.remove('active');
+
+  selectedRating.textContent = `You selected ${
+    button === null ? '0' : button.textContent
+  } out of 5`;
 });
