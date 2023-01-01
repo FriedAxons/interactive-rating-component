5bb00f6948e11b2944e8738c10014272d96f2517
FriedAxons <remo453@gmail.com>
2022-12-31 18:39:52 -0600

    Add functionality to rating buttons
    functionality added to rating buttons in order to change color on hover
    as well as on selection.

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const containerEl = document.querySelector('.container');
-const buttonRatingContainerEl = document.querySelector('.btn-rating-container');
-const ratingBtnsEl = document.querySelectorAll('.rating-btn');
+const btnRatingContainer = document.querySelector('.btn-rating-container');
+const ratingBtns = document.querySelectorAll('.rating-btn');
 const thankYouContainerEl = document.querySelector('.thank-you-container');
 const submitBtnEl = document.querySelector('.submit-btn');
 const selectedRatingEl = document.querySelector('.selected-rating');
+
+btnRatingContainer.addEventListener('click', (event) => {
+  // Get the clicked button
+  const button = event.target;
+
+  // Remove the active class from all buttons
+  ratingBtns.forEach((btn) => {
+    btn.classList.remove('active');
+  });
+
+  button.classList.add('active');
+});
